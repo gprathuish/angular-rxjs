33b76ec84b653d8f5654180d8a6b9da94b669cc6
Prathuish Gera <gprathuis@gmail.com>
2019-05-23 07:40:13 +0530

    observable init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-rxjs';
+  observable;
+  constructor(){
+    
+  }
+  
+  ngOnInit(){ this.createObservable(); }
+  
+
+  createObservable(){
+    this.observable = new Observable(subscriber => {
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.next(3);
+      setTimeout(() => {
+        subscriber.next(4);
+        subscriber.complete();
+      }, 1000);
+    }).pipe(filter(v => ! ((v as number) % 2)), map(v => (v as number) *2));
+
+    console.log("just before subsccribe");
+    this.observable.subscribe({
+      next(x) { console.log('got value ' + x); },
+      error(err) { console.error('something wrong occurred: ' + err); },
+      complete() { console.log('done'); }
+    });
+    console.log('just after subscribe');
+  }
+
+  
+
 }
